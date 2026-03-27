964d8b3fde2745dd40e5dc98bf806549d367a8bc
Juliette Bénon <juliette.benon@econ.uzh.ch>
2026-03-27 10:33:31 +0100

    Plot and report stats for robustness to neural noise

--- src/plots/subplots/plotAlternativeRobustnessMeasurePerFit.m
+++ src/plots/subplots/plotAlternativeRobustnessMeasurePerFit.m
@@ -1,9 +1,13 @@
-function [] = plotRobustnessToConnectionLesionsPerFit(ax, DataIndirect, DataFranck, DataMiles, plot_options)
+function [] = plotAlternativeRobustnessMeasurePerFit(ax, measure_type, DataIndirect, DataFranck, DataMiles, plot_options)
 % Code for figure S8.
 %
 % INPUTS ------------------------------------------------------------------
 % ax : <matlab.graphics.axis.Axes 1x1>
 %     Axes handle where the plot should be drawn.
+%
+% measure_type : <string 1x1>
+%     Field indicating whether to plot the robustness to connection lesions
+%     ("impaired_connec") or to neural noise ("noise").
 %
 % DataIndirect : <struct 1x1>
 %     Structure containing model analysis results for models trained first 
@@ -30,15 +34,16 @@ function [] = plotRobustnessToConnectionLesionsPerFit(ax, DataIndirect, DataFran
 
 arguments
     ax (1, 1) matlab.graphics.axis.Axes
+    measure_type (1, 1) string {mustBeMember(measure_type, ["impaired_connec", "noise"])}
     DataIndirect (1, 1) struct
     DataFranck (1, 1) struct
     DataMiles (1, 1) struct
     plot_options.line_width (1, 1) double = 0.7
     plot_options.p_threshold (1, 1) double = 0.005
-    plot_options.line_y_coord_top (1, 1) double = 0.6099
+    plot_options.line_y_coord_top (1, 1) double = 0.999
     plot_options.line_x_shift (1, 1) double = - 0.2
-    plot_options.line_y_coord_bottom (1, 1) double = 0.59
-    plot_options.star_y_shift (1, 1) double = 0.009
+    plot_options.line_y_coord_bottom (1, 1) double = 0.9
+    plot_options.star_y_shift (1, 1) double = 0.06
     plot_options.star_size (1, 1) double = 14
 end
 
@@ -46,11 +51,21 @@ hold(ax, "on");
 
 % Aesthetics
 xlim(ax, [-0.9, 5.9]);
-ylim(ax, [0.46, 0.61]);
-yticks(ax, 0.5:0.05:0.6);
+if measure_type == "impaired_connec"
+    ylim(ax, [0.45, 0.6]);
+    yticks(ax, 0.45:0.05:0.6);
+else
+    ylim(ax, [0.5, 0.7]);
+    yticks(ax, 0.5:0.05:0.7);
+end
+y_lim = ylim();
 xticks(ax, [0:2, 3.5 + (0:2)]);
 xticklabels(ax, repmat(["Rat.", "Irrat.\newline(distort)", "Irrat.\newline(direct)"], 1, 2));
-ylabel(ax, "Tolerance to disconnections (a.u.)");
+if measure_type == "impaired_connec"
+    ylabel(ax, "Tolerance to disconnections (a.u.)");
+else
+    ylabel(ax, "Tolerance to neural noise (a.u.)");
+end
 setAxFontSize(ax);
 
 % ~ Loop over candidate RNN configurations only ~ %
@@ -58,16 +73,16 @@ for i_config = 9:10
 
     % Select data for models trained in two steps
     select_indirect = DataIndirect.config_ID == i_config;
-    prop_rational = DataIndirect.avg_prop_optimal_impaired_connec(select_indirect & DataIndirect.is_rational)';
+    prop_rational = DataIndirect.("avg_prop_optimal_" + measure_type)(select_indirect & DataIndirect.is_rational)';
     prop_rational = repmat(prop_rational, 2, 1);
-    prop_irrational = DataIndirect.avg_prop_optimal_impaired_connec(select_indirect & DataIndirect.is_irrational)';
+    prop_irrational = DataIndirect.("avg_prop_optimal_" + measure_type)(select_indirect & DataIndirect.is_irrational)';
 
     % Select data for models trained directly on irrational behaviour
     select_direct_Franck = DataFranck.config_ID == i_config;
     select_direct_Miles = DataMiles.config_ID == i_config;
     prop_irrational_direct = [...
-        DataFranck.avg_prop_optimal_impaired_connec(select_direct_Franck & DataFranck.is_irrational)' ; ...
-        DataMiles.avg_prop_optimal_impaired_connec(select_direct_Miles & DataMiles.is_irrational)'];
+        DataFranck.("avg_prop_optimal_" + measure_type)(select_direct_Franck & DataFranck.is_irrational)' ; ...
+        DataMiles.("avg_prop_optimal_" + measure_type)(select_direct_Miles & DataMiles.is_irrational)'];
 
     % Plot distance distributions
     customViolinplot(ax, ...
@@ -83,14 +98,14 @@ for i_config = 9:10
     % Rational vs. irrational (distort)
     [~, p] = ttest(prop_rational, prop_irrational);
     % Horizontal line
-    line_y_coord = plot_options.line_y_coord_bottom;
+    line_y_coord = y_lim(1) + plot_options.line_y_coord_bottom * diff(y_lim);
     x_coord = plot_options.line_x_shift + (i_config - 9) * 3.5 + [0, 1];
     plot(ax, x_coord, line_y_coord * ones(1, 2), ...
         Color=defineModelColor(i_config), ...
         LineWidth=plot_options.line_width);
     % Star
     x_star = plot_options.line_x_shift + 0.5 + (i_config - 9) * 3.5;
-    y_star = line_y_coord - plot_options.star_y_shift;
+    y_star = line_y_coord - plot_options.star_y_shift * diff(y_lim);
     if p < plot_options.p_threshold
         text(ax, x_star, y_star, "*", ...
             HorizontalAlignment="center", ...
@@ -101,14 +116,14 @@ for i_config = 9:10
     % Rational vs. irrational (direct)
     [~, p] = ttest(prop_rational, prop_irrational_direct);
     % Horizontal line
-    line_y_coord = plot_options.line_y_coord_top;
+    line_y_coord = y_lim(1) + plot_options.line_y_coord_top * diff(y_lim);
     x_coord = plot_options.line_x_shift + (i_config - 9) * 3.5 + [0, 2];
     plot(ax, x_coord, line_y_coord * ones(1, 2), ...
         Color=defineModelColor(i_config), ...
         LineWidth=plot_options.line_width);
     % Star
     x_star = plot_options.line_x_shift + 1.5 + (i_config - 9) * 3.5;
-    y_star = line_y_coord - plot_options.star_y_shift;
+    y_star = line_y_coord - plot_options.star_y_shift * diff(y_lim);
     if p < plot_options.p_threshold
         text(ax, x_star, y_star, "*", ...
             HorizontalAlignment="center", ...
